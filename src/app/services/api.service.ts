bb756a5c0fdf29aec1026d65a9ae6a67cdcccd02
aadeshshetty <95965673+aadeshshetty@users.noreply.github.com>
2024-05-11 14:05:54 +0530

    Update backend url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  url = 'https://home-services-api.onrender.com/'
+  url = 'https://home-services-api.onrender.com'
   constructor(private http: HttpClient) { }
 
   getLocation(){
